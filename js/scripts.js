afae2fdd7f22c9c4ac52c19afa4d2ecb1c8debd8
Eddie Harris and Nick Rogers <eddie@>
2018-06-07 16:55:24 -0700

    Add additional things.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,50 @@
-var someString = "hello world this is a compute"
-var lengthString = someString.split(" ").join("").length;
-var inputString = someString.split(" ").join("").split("");
+$(document).ready(function(){
+  var newString = "";
+  function cryptoConvert(string) {
+    //FUNCTION THAT DETERMINES LENGTH OF STRING
+    var lengthString = string.split(" ").join("").length;
 
-var root = parseInt(Math.sqrt(lengthString));
-var arr = [];
+    //FUNCTION THAT SPLITS THE STRING INTO CHARACTERS IN AN ARRAY WITHOUT SPACES
+    var arrayOfCharacters = string.split(" ").join("").split("");
 
+    //FUNCTION THAT DETERMINES SIZE OF GRID
+    var root = parseInt(Math.sqrt(lengthString));
+    var row = root;
+    var column = root;
 
+    if (row*column < lengthString) {
+    	row = row + 1;
+    }
+    if (row*column < lengthString) {
+    	column = column + 1;
+    }
+    console.log(row);
+    console.log(column);
+    //FUNCTION THAT SEPARATES THE ARRAY OF CHARACTERS INTO ROWS IN ANOTHER ARRAY
+    var arrayOfRows = [];
+    for (i = 0; i < row; i++) {
+       arrayOfRows.push(arrayOfCharacters.slice(i * row, (i+1)*row));
+     }
 
-row = root;
-column = root;
-
-if (row*column < lengthString) {
-	row = row + 1;
-}
-if (row*column < lengthString) {
-	column = column + 1;
-}
-
-
-for (i = 0; i < row; i++) {
-   arr.push(inputString.slice(i * row, (i+1)*row));
-document.getElementById("test").innerHTML = arr;
- }
-
-
-  var testArr = [];
- for (b = 0; b < column; b ++) {
- for (a = 0; a < row; a ++) {
-     testArr.push(arr[a][b]);
- }
-//for (b = 0; b < column; b ++) {
-
-   }
-
-
- document.getElementById("test2").innerHTML = testArr.join("");
+    //FUNCTION THAT SEPARATES THE COLUMNS INTO AN ARRAY
+    var arrayOfColumns = [];
+   	for (b = 0; b < column; b ++) {
+   		for (a = 0; a < row; a ++) {
+       	arrayOfColumns.push(arrayOfRows[a][b]);
+   		}
+    }
+    //FUNCTION THAT SPLITS THE NEW MESSAGE INTO 5 LETTER STRINGS SEPARATED BY A SPACE
+    var arrayOfMessage = [];
+    for (i = 0; i < arrayOfColumns.length / 5; i++) {
+      arrayOfMessage.push(arrayOfColumns.slice(i * 5, (i+1)*5).join(""));
+      arrayOfMessage.push(" ");
+      }
+    newString = arrayOfMessage.join(" ");
+  }
+  $("#form-cryptosquare").submit(function(event){
+    event.preventDefault();
+    var userInput = $("input#sentence").val();
+    cryptoConvert(userInput);
+    $("#crypto-conversion").text(newString);
+  });
+});
