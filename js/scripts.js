0da2104901cd05399a123d4e70ff859776de3705
Nick Rogers <amellocello@msn.com>
2018-06-07 22:18:53 -0700

    Create working converter to cryptobox code with table.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
   var newString = "";
+  var arrayOfRows = [];
+  var arrayOfColumns = [];
+  var arrayOfMessage = [];
+  var row = 0;
+  var column = 0;
+  var alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  var filteredString = "";
   function cryptoConvert(string) {
+    //FUNCTIONS THAT REMOVE THE SYMBOLS AND SPECIAL CHARACTERS FROM THE STRING
+    filteredString = "";
+    for (i = 0; i < string.length; i++) {
+      if (alpha.includes(string.charAt(i))) {
+        filteredString = filteredString + string.charAt(i);
+      }
+    }
+    string = filteredString.toLowerCase();
     //FUNCTION THAT DETERMINES LENGTH OF STRING
     var lengthString = string.split(" ").join("").length;
 
@@ -9,8 +24,8 @@ $(document).ready(function(){
 
     //FUNCTION THAT DETERMINES SIZE OF GRID
     var root = parseInt(Math.sqrt(lengthString));
-    var row = root;
-    var column = root;
+    row = root;
+    column = root;
 
     if (row*column < lengthString) {
     	row = row + 1;
@@ -21,21 +36,23 @@ $(document).ready(function(){
     console.log(row);
     console.log(column);
     //FUNCTION THAT SEPARATES THE ARRAY OF CHARACTERS INTO ROWS IN ANOTHER ARRAY
-    var arrayOfRows = [];
+    // var arrayOfRows = [];
     for (i = 0; i < row; i++) {
-       arrayOfRows.push(arrayOfCharacters.slice(i * row, (i+1)*row));
+       arrayOfRows.push(arrayOfCharacters.slice(i * column, (i+1)*column));
      }
 
     //FUNCTION THAT SEPARATES THE COLUMNS INTO AN ARRAY
-    var arrayOfColumns = [];
+    arrayOfColumns = [];
    	for (b = 0; b < column; b ++) {
    		for (a = 0; a < row; a ++) {
+        if (arrayOfRows[a][b] !== undefined) {
        	arrayOfColumns.push(arrayOfRows[a][b]);
-   		}
+        }
+      }
     }
     //FUNCTION THAT SPLITS THE NEW MESSAGE INTO 5 LETTER STRINGS SEPARATED BY A SPACE
-    var arrayOfMessage = [];
-    for (i = 0; i < arrayOfColumns.length / 5; i++) {
+    arrayOfMessage = [];
+    for (i = 0; i < (arrayOfColumns.length / 5) + 1; i++) {
       arrayOfMessage.push(arrayOfColumns.slice(i * 5, (i+1)*5).join(""));
       arrayOfMessage.push(" ");
       }
@@ -44,7 +61,25 @@ $(document).ready(function(){
   $("#form-cryptosquare").submit(function(event){
     event.preventDefault();
     var userInput = $("input#sentence").val();
+    arrayOfRows = [];
+    arrayOfColumns = [];
+    arrayOfMessage =[];
+    filteredString = "";
     cryptoConvert(userInput);
+    // console.log(arrayOfRows);
+    // console.log(arrayOfColumns);
+    // console.log(arrayOfMessage);
+    console.log(filteredString);
+    $("#table-test").empty();
+    for(i = 0; i < row; i++) {
+      $("#table-test").append("<tr>");
+      for (j = 0; j < column; j++) {
+        if (arrayOfRows[i][j] !== undefined) {
+        $("#table-test").append("<td>" + arrayOfRows[i][j] + "</td>");
+        }
+      }
+      $("#table-test").prepend("</tr>");
+    }
     $("#crypto-conversion").text(newString);
   });
 });
